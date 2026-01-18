69ed4e2827f4b2dea4e8eb0f77c9f0eae2ad5fca
Bala <balaramu1703@gmail.com>
2026-01-18 22:14:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,14 +23,7 @@ app.get("/", (req, res) => {
 
 // Webhook verification
 app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    return res.status(200).send(challenge);
-  }
-  return res.sendStatus(403);
+  res.status(200).send(req.query["hub.challenge"] || "OK");
 });
 
 // Webhook receiver
