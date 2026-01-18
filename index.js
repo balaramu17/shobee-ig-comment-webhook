185238c0e49a74575a001a788f4418073b2a0a30
Bala <balaramu1703@gmail.com>
2026-01-18 17:07:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/webhook", (req, res) => {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "shobee_verify_123";
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
